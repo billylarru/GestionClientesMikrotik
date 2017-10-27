2809afa026546288d652ab7be2067b073aabd063
Billy Larru <larru91@gmail.com>
2017-10-26 23:46:26 -0500

    registrando pagos

--- src/main/java/com/larrunet/gui/IFrm_RegistrarPago.java
+++ src/main/java/com/larrunet/gui/IFrm_RegistrarPago.java
@@ -5,24 +5,53 @@
  */
 package com.larrunet.gui;
 
+import com.larrunet.bean.Pago;
+import com.larrunet.bean.PagoParcial;
 import com.larrunet.bean.Servicio;
+import com.larrunet.dao.PagoDAO;
 import com.larrunet.util.IFrameListener;
+import com.larrunet.util.Singleton;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author kael
  */
 public class IFrm_RegistrarPago extends javax.swing.JInternalFrame {
-    
-    
-    
+
     Servicio servicio;
+    PagoDAO daopagos;
     /**
      * Creates new form IFrm_RegistrarPago
      */
     public IFrm_RegistrarPago() {
         initComponents();
+        initObjects();
         this.addInternalFrameListener(new IFrameListener());
+        cargarDatos();
+    }
+
+    public void initObjects(){
+        daopagos = new PagoDAO();
+    }
+    
+    private void cargarDatos() {
+        Date fechaActual = new Date();
+        dtpFechaInicio.setDate(fechaActual);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.MONTH, 1);
+        Date fechaVenc = calendar.getTime();
+
+        dtpFechaVenc.setDate(fechaVenc);
+
     }
 
     /**
@@ -45,7 +74,7 @@ public class IFrm_RegistrarPago extends javax.swing.JInternalFrame {
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        dtpInicio = new org.jdesktop.swingx.JXDatePicker();
+        dtpFechaInicio = new org.jdesktop.swingx.JXDatePicker();
         dtpFechaVenc = new org.jdesktop.swingx.JXDatePicker();
         jLabel3 = new javax.swing.JLabel();
         txtMontoPagar = new javax.swing.JTextField();
@@ -127,6 +156,12 @@ public class IFrm_RegistrarPago extends javax.swing.JInternalFrame {
 
         jLabel3.setText("Monto a pagar:");
 
+        txtMontoPagar.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtMontoPagarKeyReleased(evt);
+            }
+        });
+
         jLabel5.setText("Monto recibido:");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
@@ -147,7 +182,7 @@ public class IFrm_RegistrarPago extends javax.swing.JInternalFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addComponent(dtpFechaVenc, javax.swing.GroupLayout.DEFAULT_SIZE, 160, Short.MAX_VALUE)
-                            .addComponent(dtpInicio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(dtpFechaInicio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addGroup(jPanel2Layout.createSequentialGroup()
                                 .addComponent(txtMontoPagar)
                                 .addGap(77, 77, 77))))
@@ -163,7 +198,7 @@ public class IFrm_RegistrarPago extends javax.swing.JInternalFrame {
                 .addGap(17, 17, 17)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(dtpInicio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(dtpFechaInicio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
@@ -180,6 +215,11 @@ public class IFrm_RegistrarPago extends javax.swing.JInternalFrame {
         );
 
         btnRegistrarPago.setText("Registrar pago");
+        btnRegistrarPago.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegistrarPagoActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -193,7 +233,7 @@ public class IFrm_RegistrarPago extends javax.swing.JInternalFrame {
                 .addContainerGap())
             .addGroup(layout.createSequentialGroup()
                 .addGap(191, 191, 191)
-                .addComponent(btnRegistrarPago, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btnRegistrarPago, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -204,8 +244,8 @@ public class IFrm_RegistrarPago extends javax.swing.JInternalFrame {
                 .addGap(34, 34, 34)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(46, 46, 46)
-                .addComponent(btnRegistrarPago, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(60, Short.MAX_VALUE))
+                .addComponent(btnRegistrarPago, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(28, Short.MAX_VALUE))
         );
 
         pack();
@@ -217,20 +257,72 @@ public class IFrm_RegistrarPago extends javax.swing.JInternalFrame {
         dialog.setVisible(true);
     }//GEN-LAST:event_btnBuscarActionPerformed
 
-    
-    public void mostrarServicio(Servicio servicio){
+    private void txtMontoPagarKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtMontoPagarKeyReleased
+        txtMontoRecibido.setText(txtMontoPagar.getText());
+    }//GEN-LAST:event_txtMontoPagarKeyReleased
+
+    private void btnRegistrarPagoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarPagoActionPerformed
+        if (servicio != null) {
+            Pago pago = new Pago();
+
+            double montoPago = Double.parseDouble(txtMontoPagar.getText());
+            double montoRecibido = Double.parseDouble(txtMontoRecibido.getText());
+
+            if (montoPago == montoRecibido) {
+                pago.setEstadoPago("CANCELADO");
+            } else {
+                pago.setEstadoPago("POR PAGAR");
+                
+                PagoParcial pagoParcial = new PagoParcial();
+                pagoParcial.setMonto(montoRecibido);
+                pagoParcial.setFechaPago(LocalDateTime.now());
+                pagoParcial.setPersonal(Singleton.getInstance().getPersonal());
+                pagoParcial.setEstadoPagoParcial("CANCELADO");
+                
+                pago.addPagoParcial(pagoParcial);
+            }
+            
+            pago.setFechaEmision(LocalDateTime.now());
+            pago.setFechaInicio(dateToLocalDate(dtpFechaInicio.getDate()));
+            pago.setFechaVencimiento(dateToLocalDate(dtpFechaVenc.getDate()));
+            pago.setMontoPago(montoPago);
+            
+            pago.setPersonal(Singleton.getInstance().getPersonal());
+            pago.setCliente(servicio.getCliente());
+
+            
+           if(daopagos.registrarPago(pago)){
+               JOptionPane.showMessageDialog(this, "¡Pago registrado correctamente!", "REGISTRO SATISFACTORIO", JOptionPane.INFORMATION_MESSAGE);
+           }else{
+               JOptionPane.showMessageDialog(this, "¡No se pudo registrar el pago!", "ERROR AL REGISTRAR PAGO", JOptionPane.ERROR_MESSAGE);
+           }
+        } else {
+            JOptionPane.showMessageDialog(this, "Especifique el cliente al que se le va a cobrar", "FALTA CLIENTE", JOptionPane.ERROR_MESSAGE);
+        }
+    }//GEN-LAST:event_btnRegistrarPagoActionPerformed
+
+    public void mostrarServicio(Servicio servicio) {
         this.servicio = servicio;
         txtNombres.setText(servicio.getCliente().getNombresCompletos());
         txtPagoMensual.setText(Double.toString(servicio.getPagoMensual()));
         txtEstado.setText(servicio.getEstadoServicio());
     }
-    
+
+    public LocalDate dateToLocalDate(Date date) {
+        LocalDate localDate = null;
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+
+        localDate = LocalDate.of(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+
+        return localDate;
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnBuscar;
     private javax.swing.JButton btnRegistrarPago;
+    private org.jdesktop.swingx.JXDatePicker dtpFechaInicio;
     private org.jdesktop.swingx.JXDatePicker dtpFechaVenc;
-    private org.jdesktop.swingx.JXDatePicker dtpInicio;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
