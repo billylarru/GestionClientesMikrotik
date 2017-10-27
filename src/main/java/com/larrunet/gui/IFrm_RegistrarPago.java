74ef2477a196b5ae49e1e4f972c38a0dab1877a7
Billy Larru <larru91@gmail.com>
2017-10-26 23:49:57 -0500

    faltaba sumar 1 unidad al mes de registro de pago

--- src/main/java/com/larrunet/gui/IFrm_RegistrarPago.java
+++ src/main/java/com/larrunet/gui/IFrm_RegistrarPago.java
@@ -313,7 +313,7 @@ public class IFrm_RegistrarPago extends javax.swing.JInternalFrame {
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
 
-        localDate = LocalDate.of(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+        localDate = LocalDate.of(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH)+1, cal.get(Calendar.DAY_OF_MONTH));
 
         return localDate;
     }
