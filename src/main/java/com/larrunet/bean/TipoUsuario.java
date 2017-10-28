e9e03cbe299d3b33f752d31ec94dfda57ed7fa6a
Billy Larru <larru91@gmail.com>
2017-10-28 11:39:37 -0500

    generando getters y setters en el bean TipoUsuario

--- src/main/java/com/larrunet/bean/TipoUsuario.java
+++ src/main/java/com/larrunet/bean/TipoUsuario.java
@@ -34,5 +34,53 @@ public class TipoUsuario implements Serializable {
     
     @OneToMany(mappedBy = "tipoUsuario")
     private List<Usuario> usuarios = new ArrayList<>();
+
+    public TipoUsuario() {
+    }
+
+    public TipoUsuario(Integer idTipoUsuario, String descripTipoUsuario, String estadoTipoUsuario) {
+        this.idTipoUsuario = idTipoUsuario;
+        this.descripTipoUsuario = descripTipoUsuario;
+        this.estadoTipoUsuario = estadoTipoUsuario;
+    }
+
+    public Integer getIdTipoUsuario() {
+        return idTipoUsuario;
+    }
+
+    public void setIdTipoUsuario(Integer idTipoUsuario) {
+        this.idTipoUsuario = idTipoUsuario;
+    }
+
+    public String getDescripTipoUsuario() {
+        return descripTipoUsuario;
+    }
+
+    public void setDescripTipoUsuario(String descripTipoUsuario) {
+        this.descripTipoUsuario = descripTipoUsuario;
+    }
+
+    public String getEstadoTipoUsuario() {
+        return estadoTipoUsuario;
+    }
+
+    public void setEstadoTipoUsuario(String estadoTipoUsuario) {
+        this.estadoTipoUsuario = estadoTipoUsuario;
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    @Override
+    public String toString() {
+        return "TipoUsuario{" + "idTipoUsuario=" + idTipoUsuario + ", descripTipoUsuario=" + descripTipoUsuario + ", estadoTipoUsuario=" + estadoTipoUsuario + '}';
+    }
+    
+    
     
 }
